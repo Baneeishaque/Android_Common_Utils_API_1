7359adffb339196bfb3b9dc08d31011ca9652c7e
Banee Ishaque K <baneeishaque@gmail.com>
2019-04-20 14:13:17 +0530

    DisplayHelper

--- common/src/main/java/ndk/utils_android1/ActivityUtils.java
+++ common/src/main/java/ndk/utils_android1/ActivityUtils.java
@@ -3,7 +3,7 @@ package ndk.utils_android1;
 import android.content.Context;
 import android.content.Intent;
 
-public class Activity_Utils {
+public class ActivityUtils {
 
     public static void start_activity(Context context, Class activity) {
         Intent intent = new Intent(context, activity);
