a34a95eb8dd83991501ad44b6773e631f2733932
Banee Ishaque K <baneeishaque@gmail.com>
2020-04-20 00:38:57 +0000

    optimize

--- common1/src/main/java/ndk/utils_android1/TelephonyCallActivity.java
+++ common1/src/main/java/ndk/utils_android1/TelephonyCallActivity.java
@@ -17,37 +17,40 @@ public abstract class TelephonyCallActivity extends TelephonyCallPermissionActiv
 
             Intent callIntent = new Intent(Intent.ACTION_CALL);
             callIntent.setData(Uri.parse("tel:" + mobileNumber));
-
-            getRuntimePermission(new PermissionGrantedActions() {
-
-                @SuppressLint("MissingPermission")
-                @Override
-                public void configurePermissionGrantedActions() {
-
-                    context.startActivity(callIntent);
-                }
-
-            }, new PermissionAcceptedActions() {
-
-                @SuppressLint("MissingPermission")
-                @Override
-                public void configurePermissionAcceptedActions() {
-
-                    context.startActivity(callIntent);
-                }
-
-            }, new PermissionDeniedActions() {
-
-                @Override
-                public void configurePermissionDeniedActions() {
-
-                    Toast.makeText(activityContext, R.string.call_permission_required, Toast.LENGTH_SHORT).show();
-                }
-            });
-
+            executePermissionProcedure(context, callIntent);
             return true;
         }
-
         return false;
+    }
+
+    private void executePermissionProcedure(Context context, Intent callIntent) {
+
+        getRuntimePermission(new PermissionGrantedActions() {
+
+            @SuppressLint("MissingPermission")
+            @Override
+            public void configurePermissionGrantedActions() {
+
+                context.startActivity(callIntent);
+            }
+
+        }, new PermissionAcceptedActions() {
+
+            @SuppressLint("MissingPermission")
+            @Override
+            public void configurePermissionAcceptedActions() {
+
+                context.startActivity(callIntent);
+            }
+
+        }, new PermissionDeniedActions() {
+
+            @Override
+            public void configurePermissionDeniedActions() {
+
+                Toast.makeText(activityContext, R.string.call_permission_required, Toast.LENGTH_SHORT).show();
+                executePermissionProcedure(context, callIntent);
+            }
+        });
     }
 }
